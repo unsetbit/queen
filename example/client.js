f84a27b2371c526f43180eb9c341069d7f427788
Ozan Turgut <ozanturgut@gmail.com>
2013-01-16 01:44:39 -0800

    Updated examples
    Signed-off-by: Ozan Turgut <ozanturgut@gmail.com>

---
+++ example/client.js
@@ -0,0 +1,19 @@
+// "queenSocket" is a global variable that scripts running on queen have
+// access to. It's the only variable introduced to this scope. This script 
+// will execute in it's own iframe so the global context is clean otherwise.
+
+// The "onMessage" property allows the client-side script to listen
+// to messages from the server-side script. In this case we're assuming
+// the server script will only send us one message, and it'll be an integer
+// for the maximum number we're should guess.
+queenSocket.onMessage = function(maxNumber){
+	function guess(){
+		var number = Math.floor(Math.random() * maxNumber);
+
+		// This function sends a message back to the server-side script
+		queenSocket(number);	
+	}
+
+	// Every 100ms, make a guess
+	setInterval(guess, 100);
+};
---
+++ example/server.js
@@ -0,0 +1,49 @@
+/*
+Chance Example
+
+This example shows an example of a distributed problem solving in queen.
+Any browser which connects will begin guessing numbers under "maxNumber",
+once any of the browsers find the "numberToFind", all workers will be killed,
+and the process will exit.
+
+The process will continue to run until one browser guesses the right number,
+if no browsers are connected, it'll idle and wait.
+
+*/
+
+var numberToFind = 42,
+	maxNumber = 300,
+	workforceConfig,
+	workforce;
+
+// Run this script on the client-side (i.e. browser).
+workforceConfig.run = ['http://queenjs.com/client-example.js'];
+
+// If a browser connects after we started, have them join the workforce
+workforceConfig.populate = "continuous";
+
+// Don't kill the workforce if it's idling with no browsers connected
+workforceConfig.killOnStop = false;
+
+// When a new worker (i.e. browser) connects, trigger this handler function
+workforceConfig.handler = function(worker){
+
+	// Tell the worker the maximum number to guess
+	worker(maxNumber);
+	
+	// Whenever a worker has a message, execute a handler function
+	worker.on("message", function(guessedNumber){
+		console.log(guessedNumber + " \t guessed by" + worker.provider.attributes.name);
+
+		if(guessedNumber === numberToFind){
+			workforce.kill();	
+			console.log("Done! The winner was " + worker.provider.attributes.name);
+		
+			// Exit the entire process
+			queen.kill()
+		}
+	});
+};
+
+// Create the workforce
+workforce = queen(workforceConfig);
