9266ff09f770a7665ef3af0cf4bfd2f6f4987811
Ozan Turgut <ozanturgut@gmail.com>
2013-01-20 13:32:19 -0800

    Changed example
    Signed-off-by: Ozan Turgut <ozanturgut@gmail.com>

--- server-example.js
+++ server-example.js
@@ -10,40 +10,40 @@ The process will continue to run until one browser guesses the right number,
 if no browsers are connected, it'll idle and wait.
 
 */
+module.exports = function(queen){
+	var numberToFind = 42,
+		maxNumber = 300,
+		workforceConfig = {},
+		workforce;
 
-var numberToFind = 42,
-	maxNumber = 300,
-	workforceConfig = {},
-	workforce;
+	// Run this script on the client-side (i.e. browser).
+	workforceConfig.run = ['http://queenjs.com/client-example.js'];
 
-// Run this script on the client-side (i.e. browser).
-workforceConfig.run = ['http://queenjs.com/client-example.js'];
+	// If a browser connects after we started, have them join the workforce
+	workforceConfig.populate = "continuous";
 
-// If a browser connects after we started, have them join the workforce
-workforceConfig.populate = "continuous";
+	// Don't kill the workforce if it's idling with no browsers connected
+	workforceConfig.killOnStop = false;
 
-// Don't kill the workforce if it's idling with no browsers connected
-workforceConfig.killOnStop = false;
-
-// When a new worker (i.e. browser) connects, trigger this handler function
-workforceConfig.handler = function(worker){
-
-	// Tell the worker the maximum number to guess
-	worker(maxNumber);
-	
-	// Whenever a worker has a message, execute a handler function
-	worker.on("message", function(guessedNumber){
-		console.log(guessedNumber + " \t guessed by " + worker.provider.attributes.name);
-
-		if(guessedNumber === numberToFind){
-			workforce.kill();	
-			console.log("Done! The winner was " + worker.provider.attributes.name);
+	// When a new worker (i.e. browser) connects, trigger this handler function
+	workforceConfig.handler = function(worker){
+		// Tell the worker the maximum number to guess
+		worker(maxNumber);
 		
-			// Exit the entire process
-			process.exit(0);
-		}
-	});
-};
+		// Whenever a worker has a message, execute a handler function
+		worker.on("message", function(guessedNumber){
+			console.log(guessedNumber + " \t guessed by " + worker.provider.attributes.name);
 
-// Create the workforce
-workforce = queen(workforceConfig);
+			if(guessedNumber === numberToFind){
+				workforce.kill();	
+				console.log("Done! The winner was " + worker.provider.attributes.name);
+			
+				// We're done
+				queen.kill();
+			}
+		});
+	};
+
+	// Create the workforce
+	workforce = queen(workforceConfig);	
+};
