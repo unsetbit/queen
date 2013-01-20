e4d5fe4aa4ca84dca28d81b538afd617ddb6d866
Ozan Turgut <ozanturgut@gmail.com>
2013-01-20 14:17:34 -0800

    Updated sauce example
    Signed-off-by: Ozan Turgut <ozanturgut@gmail.com>

--- example/sauce/queenConfig.js
+++ example/sauce/queenConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-	script: "http://queenjs.com/server-example.js"; // script to run on load
+	script: "http://queenjs.com/server-example.js", // script to run on load
 	populator: {
 		type: "sauce",
 		config: {
@@ -11,6 +11,46 @@ module.exports = {
 				browserName: "firefox",
 				platform: "Windows 2003",
 				version: "18"
+			},
+			{ 
+				browserName: "firefox",
+				platform: "Windows 2003",
+				version: "17"
+			},
+			{ 
+				browserName: "firefox",
+				platform: "Windows 2003",
+				version: "16"
+			},
+			{ 
+				browserName: "firefox",
+				platform: "Windows 2003",
+				version: "15"
+			},
+			{ 
+				browserName: "firefox",
+				platform: "Windows 2003",
+				version: "14"
+			},
+			{ 
+				browserName: "firefox",
+				platform: "Windows 2003",
+				version: "13"
+			},
+			{ 
+				browserName: "firefox",
+				platform: "Windows 2003",
+				version: "12"
+			},
+			{ 
+				browserName: "firefox",
+				platform: "Windows 2003",
+				version: "11"
+			},
+			{ 
+				browserName: "firefox",
+				platform: "Windows 2003",
+				version: "10"
 			}
 		]
 	}
